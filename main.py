ea7ba1e16af1d97451a6c0bace2fea42da651acf
Matthew Heeter <matthewheeter1611@gmail.com>
2023-04-19 22:32:29 -0400

    Stuff

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 n = 4
+# Creating array of ones to track each combination
+combos = []
+for i in range(n):
+    combos.append(1)
+
+# Using nested for loops to do this
+# combos = 1
+# for combo in combos:
+#     for stair in n:
+
+
+
+# Ones array only
