bcf2c6cc7924858257a547f6351701147972b22f
Matthew Heeter <matthewheeter1611@gmail.com>
2023-04-20 20:30:41 -0400

    Prelim. result calc added

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def stairs(n):
         # Starting at one as every number will have one grouping with n steps (all singles)
         result = 1
     # 
-    
+    result += (n-1) + (n-1)
 
     return result
 print(stairs(n))
